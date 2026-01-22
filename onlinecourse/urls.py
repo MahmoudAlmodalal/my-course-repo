4dd08a30299d969ca8f09b5e9991d20f1a329551
MahmoudAlmodalal <132272679+MahmoudAlmodalal@users.noreply.github.com>
2026-01-22 19:09:43 +0200

    Update urls.py

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -4,22 +4,26 @@ from django.conf.urls.static import static
 from . import views
 
 app_name = 'onlinecourse'
+
 urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
+    # Index page showing the list of courses
     path(route='', view=views.CourseListView.as_view(), name='index'),
+    
+    # Authentication routes
     path('registration/', views.registration_request, name='registration'),
     path('login/', views.login_request, name='login'),
     path('logout/', views.logout_request, name='logout'),
-    # ex: /onlinecourse/5/
+    
+    # Course detail view (where lessons and the exam are displayed)
     path('<int:pk>/', views.CourseDetailView.as_view(), name='course_details'),
-    # ex: /enroll/5/
+    
+    # Enrollment route
     path('<int:course_id>/enroll/', views.enroll, name='enroll'),
 
-    # ex: /onlinecourse/5/submit
+    # <HINT> Requirement: Route for submitting the exam
     path('<int:course_id>/submit/', views.submit, name='submit'),
-    # ex: /onlincourse/5/submission/1/result/
-    path('<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result,
-         name='show_exam_result'),
- ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+    # <HINT> Requirement: Route for showing the exam result
+    path('<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name='show_exam_result'),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
